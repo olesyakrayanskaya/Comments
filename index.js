720ab3678351c8233af1979ca75102997ee1ff0a
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2023-03-16 23:23:10 +0300

    js isLiked function

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('hi');
+document.addEventListener('DOMContentLoaded', function () {
+
+    let comments = [];
+
+});
+
+function isLiked() {
+    let noLiked = document.querySelector('.comments__icon_noliked');
+    let liked = document.querySelector('.comments__icon_liked');
+    let likesCounter = document.querySelector('.comments__like-count');
+
+    noLiked.addEventListener('click', function () {
+        noLiked.classList.add('hide');
+        liked.classList.remove('hide');
+        let count = Number(likesCounter.innerHTML) + 1;
+        likesCounter.innerHTML = count;
+    });
+
+    liked.addEventListener('click', function () {
+        noLiked.classList.remove('hide');
+        liked.classList.add('hide');
+        let count = Number(likesCounter.innerHTML) - 1;
+        likesCounter.innerHTML = count;
+    });
+};
+
+isLiked();
+
+function deleteComment () {
+    let deletBtn = document.querySelector('.comments__icon_del');
+    let comment = document.querySelector('.comments__item');
+};
